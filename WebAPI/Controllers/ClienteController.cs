86a28338b55470c881e437a36ea86e29f9286392
Oz <osvaldogioia@hotmail.com>
2020-11-21 11:30:31 -0300

    Initial commit

---
+++ WebAPI/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        List<Cliente> listacliente = new List<Cliente>();
+
+
+        public ClienteController()
+        {
+            Cliente c = new Cliente { IDCliente = 1, Nombre = "Lucia", Mail = "lucia@gmail.com" };
+
+            this.listacliente.Add(c);
+        }
+
+        //GET api/Cliente
+        [HttpGet]
+        public List<Cliente> GetClientes()
+        {
+            return this.listacliente;
+        }
+
+
+        //GET api/Cliente/9
+        [HttpGet("{id}")]
+        public Cliente GetCliente(int id)
+        {
+            Cliente c = this.listacliente.Find(z => z.IDCliente == id);
+
+            return c;
+        }
+    }
+}
---
+++ WebAPI/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Cliente
+    {
+        public int IDCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Cuit_DNI { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Mail { get; set; }
+
+        public int IDTipoCliente { get; set; }
+
+        public string Observacion { get; set; }
+
+        public string Direccion { get; set; }
+
+        public bool Borrado { get; set; }
+
+    }
+}
